e8a492cc00133244404c3e8aefe3a28a0433dfef
ALEMDJOU <fofackhenri36@gmail.com>
2026-04-22 00:17:08 +0100

    modification de config.js pour la production

--- sparktime_frontend/assets/js/config.js
+++ sparktime_frontend/assets/js/config.js
@@ -2,5 +2,5 @@ const isLocal = window.location.hostname === "localhost" || window.location.host
 
 // Configuration globale dynamique pour un déploiement simple (0 configuration)
 export const APP_CONFIG = {
-    API_BASE_URL: isLocal ? "http://127.0.0.1:8000" : "https://api.sparktime.app" // En production, mettez l'URL de votre API Render/Heroku etc
+    API_BASE_URL: isLocal ? "http://127.0.0.1:8000" : "https://pre-hackverse-three-as-one.onrender.com" // En production, mettez l'URL de votre API Render/Heroku etc
 };
